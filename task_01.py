6119b03e629d7a787aceafe582643e922dcf30dd
Maureen Kilmade <maureen.kilmade@spsmail.cuny.edu>
2014-09-24 17:33:21 -0400

    pylint errors, changed == True to 'is True'

--- task_01.py
+++ task_01.py
@@ -2,14 +2,17 @@
 # -*- coding: utf-8 -*-
 """Defining a Function"""
 
-def bool_to_str(bvalue, short = False):
+
+def bool_to_str(bvalue, short=False):
+    """Changes True/False to strings Y/N or Yes/No"""
     while short:
-        if bvalue == True:
+        if bvalue is True:
             return "Y"
         else:
             return "N"
+            break
     else:
-        if bvalue == True:
+        if bvalue is True:
             return "Yes"
         else:
             return "No"
