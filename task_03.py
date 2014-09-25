324666c7e917da942fde35875d2a01da097f915d
Maureen Kilmade <maureen.kilmade@spsmail.cuny.edu>
2014-09-25 15:14:32 -0400

    pylint errors

--- task_03.py
+++ task_03.py
@@ -5,16 +5,16 @@
 
 def celsius_to_fahrenheit(temperature):
     """convert C to F"""
-    C = temperature
-    F = (C * 9) / 5 + 32
-    return float(F)
+    c = temperature
+    f = (c * 9) / 5 + 32
+    return float(f)
 
 
 def fahrenheit_to_celsius(temperature):
     """convert F to C"""
-    F = temperature
-    C = 5 * (F - 32) / 9
-    return float(C)
+    f = temperature
+    c = 5 * (f - 32) / 9
+    return float(c)
 
 
 def convert_temperature(temperature, output_format='c'):
@@ -23,16 +23,16 @@ def convert_temperature(temperature, output_format='c'):
     temp = float(temperature[:-1])
 
     if output_format == 'f':
-    	if degree_type == 'c':
-    		return celsius_to_fahrenheit(temp)
-    	elif degree_type == 'f':
+        if degree_type == 'c':
+            return celsius_to_fahrenheit(temp)
+        elif degree_type == 'f':
             return temp
         else:
-        	return None
+            return None
     elif output_format == 'c':
-    	if degree_type == 'f':
-     		return fahrenheit_to_celsius(temp)
-    	elif degree_type == 'c':
-   	   		return temp
+        if degree_type == 'f':
+            return fahrenheit_to_celsius(temp)
+        elif degree_type == 'c':
+            return temp
     else:
-    	return None
+        return None
